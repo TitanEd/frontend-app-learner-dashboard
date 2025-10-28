14e76322ca8c943b5ae4aa1f72e4539d884fc38e
skongala-psi <skongala@primesoft.net>
2025-10-28 14:26:02 +0530

    Updated Recommended Course Card

--- src/components/Dashboard/components/RecommendedCourseCard.tsx
+++ src/components/Dashboard/components/RecommendedCourseCard.tsx
@@ -5,7 +5,9 @@ import React from 'react';
 import {
   Card, Button,
 } from '@openedx/paragon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './RecommendedCourseCard.scss';
+import { useFormatDate } from '../../../utils/hooks';
 // import { Repeat } from '@openedx/paragon/icons';
 
 type RecommendedCourseCardProps = {
@@ -16,6 +18,9 @@ type RecommendedCourseCardProps = {
   // onEditCourse?: () => void;
   // onReRun?: () => void;
   // isReRunEnabled?: boolean;
+  courseDate?: string;
+  noOfStudents?: number;
+  noOfLessons?: number;
 };
 
 const fallbackImage = 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg';
@@ -25,10 +30,14 @@ const RecommendedCourseCard: React.FC<RecommendedCourseCardProps> = ({
   title,
   metadata,
   onViewLive,
+  courseDate,
+  noOfStudents,
+  noOfLessons,
   // onEditCourse,
   // onReRun,
   // isReRunEnabled = false,
 }) => {
+  const formatDate = useFormatDate();
   const displayBannerSrc = imageSrc
     && !imageSrc.includes('images_course_image.jpg')
     && !imageSrc.includes('pencils.jpg')
@@ -36,7 +45,7 @@ const RecommendedCourseCard: React.FC<RecommendedCourseCardProps> = ({
     : null;
 
   return (
-    <Card style={{ width: '19rem' }} className="recommended-course-card">
+    <Card style={{ width: '19rem' }} className="recommended-course-card" onClick={onViewLive}>
       <Card.ImageCap
         src={displayBannerSrc === null ? fallbackImage : displayBannerSrc}
         srcAlt="Course cover"
@@ -44,13 +53,19 @@ const RecommendedCourseCard: React.FC<RecommendedCourseCardProps> = ({
       />
       <Card.Section className="text-center recommended-course-card-content-section">
         <div className="recommended-course-card-title-container">
+          <p className="recommended-course-card-date">{courseDate !== null ? formatDate(courseDate) : formatDate(new Date())}</p>
           <h6 className="recommended-course-card-title fw-bold">{title}</h6>
-          <p className="text-muted small">{metadata}</p>
         </div>
-        {/* <hr /> */}
-        {/* <p className="text-muted">{metadata}</p> */}
-        <div className="d-flex gap-2 recommended-course-card-buttons">
-          <Button size="sm" variant="primary" onClick={onViewLive}>View Course</Button>
+        <div className="recommended-course-card-metadata">
+          <p className="metadata-item">
+            <FontAwesomeIcon icon="book" className="mr-1" />
+            {noOfLessons != null ? `${noOfLessons} Lessons` : '10 Lessons'}
+          </p>
+          <p className="metadata-item">
+            <FontAwesomeIcon icon="users" className="mr-1" />
+            {noOfStudents != null ? `${noOfStudents} Students` : '32 Students'}
+          </p>
+          {/* <Button size="sm" variant="primary" onClick={onViewLive}>View Course</Button> */}
           {/* <Button size="sm" variant="outline-primary" onClick={onEditCourse}>Edit course</Button>
           {isReRunEnabled && (
           <IconButtonWithTooltip src={Repeat} size="sm" variant="primary" iconAs={Icon} alt="Re-run" tooltipPlacement="bottom" tooltipContent="Re-run" onClick={onReRun} />
--- src/utils/RegisterFontAwesome.jsx
+++ src/utils/RegisterFontAwesome.jsx
@@ -12,13 +12,13 @@ import {
 // } from '@fortawesome/free-solid-svg-icons';
 
 import {
-    faUsers, faChartBar, faTachometerAlt, faClock
-} from '@fortawesome/free-solid-svg-icons'
+  faUsers, faChartBar, faTachometerAlt, faClock, faBook,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function registerFontAwesomeIcons() {
   // Add brand icons
   library.add(faApple, faFacebook, faGoogle, faMicrosoft);
 
   // Add solid icons (for your dashboard metrics)
-  library.add(faUsers, faChartBar, faTachometerAlt, faClock);
+  library.add(faUsers, faChartBar, faTachometerAlt, faClock, faBook);
 }
